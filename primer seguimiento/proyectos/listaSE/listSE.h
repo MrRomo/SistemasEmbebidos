59e7ed87aa3358d9eb73c84e1a7b1a849bbf4e8e
Ricardo Romo <jricardoromo21@gmail.com>
2019-02-24 09:02:48 -0500

    list DEC works

---
+++ primer seguimiento/proyectos/listaSE/listSE.h
@@ -0,0 +1,50 @@
+#ifndef _DINAMICSTRUCTURE
+#define _DINAMICSTRUCTURE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//Firmas de funciones
+struct list;
+struct nodo;
+
+typedef struct nodo
+{
+    int dato;
+    struct nodo *next;
+    struct nodo *prev;
+    struct nodo *pos;
+} Nodo;
+
+typedef struct list
+{
+    size_t tam;
+    Nodo *head;
+    Nodo *tail;
+} List;
+
+//Construcctor de la lista
+List* list_new();
+
+//inserta un elemento en la posicion especifica
+//devuelve un bool si hubo un error o no
+bool list_append(List* list, void* elemento);
+
+//devuelve el valor del elemento segun el indice 
+void* list_get(List* list, size_t indice);
+
+//recorre la lista de izquierda a derecha a travez de sus nodos
+Nodo* list_node(Nodo* nodo, char move);
+
+//Recorre e imprime toda la lista
+void list_run(List* list);
+
+//obtiene el tamaño del list
+size_t list_length(List* list);
+
+//destruye el list de la memoria
+void list_destroy(List* list);
+
+
+
+
+#endif
---
+++ primer seguimiento/proyectos/listaSE/listSERun.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "listSE.c"
+void imprimirMenu();
+void desition();
+void printDesition();
+void clrscr();
+void space (char pos);
+int posMarker = 0;
+Nodo *pos = NULL;
+
+int main(int argc, char const *argv[])
+{
+    List *lista1 = list_new();
+    int salir = 0;
+
+    for (size_t i = 0; i < 10; i++)
+    {
+        list_append(lista1, &i);
+    }
+    pos = lista1->head;
+    while (1)
+    {
+        imprimirMenu();
+        fflush(stdin);
+        scanf("%d", &salir);
+        clrscr();
+        desition(salir, lista1);
+    }
+    return 0;
+}
+
+void desition(int salir, List *list)
+{
+    // printDesition();
+    switch (salir)
+    {
+    case 1:
+        printList(list);
+        break;
+    case 2:
+        printf("Ingrese el nuevo valor...");
+        int tmpNum;
+        fflush(stdin);
+        scanf("%d", &tmpNum);
+        list_append(list, &tmpNum);
+        printList(list);
+        break;
+    case 3:
+        printf("Saliendo de invitado list maker...");
+        break;
+    case 4:
+        printf("Hacia donde se desea mover a(derecha) o b(izquierda)\n");
+        pos = list_move_node(pos, 'd');
+        posMarker++;
+        if(posMarker>=list_length(list)) posMarker=0;
+        printList(list);
+        break;
+    default:
+        break;
+    }
+}
+void printList(List * list){
+    list_run(list);
+    space(posMarker*3);
+    printf("^\n");
+    space(posMarker*3);
+    printf("[%d,%d]\n", posMarker, (int)pos->dato);
+}
+void imprimirMenu()
+{
+    printf("**** Bienvenido al list maker ****\n ¿que desea hacer?\n");
+    printf("1 - Ver lista\n");
+    printf("2 - Agregar valor\n");
+    printf("3 - Editar posicion\n");
+    printf("4 - Mover posicion\n");
+    printf("5 - Eliminar posicion\n");
+
+}
+void printDesition()
+{
+    printf("**** Que Lista quiere seleccionar ****\n");
+    printf("1 - Lista 1\n");
+    printf("2 - Lista 2\n");
+}
+void clrscr()
+{
+    system("@cls||clear");
+}
+
+void space (char pos){
+    for(char i = 0; i <pos; i++)
+    {
+        printf(" ");
+    }
+}
